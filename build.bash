b1147121b68eb6699b27f8860a83c4179c09bd55
Sheldon McGrandle <developer@8cylinder.com>
2019-07-25 21:01:43 -0700

    Remove any pycache dirs when building

--- build.bash
+++ build.bash
@@ -4,6 +4,11 @@ PROJECT_ROOT="$HOME/projects/boss/boss"
 BOSS_DIR='boss'
 
 cd $PROJECT_ROOT
+
+for d in $(find -iname '*__pycache__' -type d); do
+    rm -rf "$d"
+done
+
 if python3 -m zipapp --compress --python "/usr/bin/env python3" $BOSS_DIR; then
     echo "Boss built, $(du -h $PROJECT_ROOT/boss.pyz | cut -f1)"
 fi
