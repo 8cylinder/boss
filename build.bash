82f97a60cb93ca1519b10a53e494f5904e611f2a
Sheldon McGrandle <developer@8cylinder.com>
2019-07-28 12:23:32 -0700

    rename boss dir to src so the zipapp that is generated can be called boss

--- build.bash
+++ build.bash
@@ -1,16 +1,18 @@
-# pip3 install click --target boss
+
+# To install packages for use by boss, eg:
+# pip3 install click --target boss/deps
 
 PROJECT_ROOT="$HOME/projects/boss/boss"
-BOSS_DIR='boss'
+BOSS_DIR='src'
+BOSS_APP='boss'
 
 cd $PROJECT_ROOT
 
-for d in $(find -iname '*__pycache__' -type d); do
+# remove any pycache dirs created while testing
+for d in $(find -iname '__pycache__' -type d); do
     rm -rf "$d"
 done
 
-if python3 -m zipapp --compress --python "/usr/bin/env python3" $BOSS_DIR; then
+if python3 -m zipapp --compress --python "/usr/bin/env python3" --output=$BOSS_APP $BOSS_DIR; then
     echo "Boss built, $(du -h $PROJECT_ROOT/boss.pyz | cut -f1)"
 fi
-
-
