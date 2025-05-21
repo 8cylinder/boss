f3ac823c9de079d86cb7c517eb239cedc16f2726
Sheldon McGrandle <developer@8cylinder.com>
2025-05-20 19:58:03 -0700

    Add an option to specify Ubuntu's version

--- src/boss/cli.py
+++ src/boss/cli.py
@@ -6,8 +6,7 @@ import re
 import textwrap
 import subprocess
 import socket
-import zipfile
-from pprint import pprint as pp # noqa
+from pprint import pprint as pp  # noqa
 import click
 from collections import namedtuple
 
@@ -38,13 +37,13 @@ from .mods.virtualhost import VirtualHost
 from .mods.webmin import Webmin
 from .mods.webservers import Apache2
 from .mods.webservers import Nginx
-# from mods.wordpress import Wordpress
-# from mods.wordpress import WpCli
+
 import importlib.metadata
 
+# DIST_VERSION = None
 __version__ = importlib.metadata.version("boss")
 
-DIST_VERSION: float|None = None
+DIST_VERSION: float | None = None
 
 # All the mods available in the order they should be run
 mods = (
@@ -73,7 +72,7 @@ mods = (
 
 
 def is_server(server):
-    if '.' not in server:
+    if "." not in server:
         return False
     return True
 
@@ -94,9 +93,11 @@ def is_ipaddress(ip):
 
 # ---------------------------- Custom types ----------------------------
 
+
 class Server(click.ParamType):
     """Check if a string sort of looks like a url by checking for a '.' in it"""
-    name = 'server'
+
+    name = "server"
 
     def convert(self, value, param, ctx):
         if not is_server(value):
@@ -114,14 +115,14 @@ class UserPass(click.ParamType):
 
     format: username,password"""
 
-    name = 'user_pass'
+    name = "user_pass"
 
     def convert(self, value, param, ctx):
         try:
-            username, password = [i.strip() for i in value.split(',', 1) if i.strip()]
+            username, password = [i.strip() for i in value.split(",", 1) if i.strip()]
         except ValueError:
-            msg = '''must be a username and password seperated by a comma
-            (the password can have a comma in it, but not the username).'''
+            msg = """must be a username and password seperated by a comma
+            (the password can have a comma in it, but not the username)."""
             self.fail(msg, param, ctx)
         return username.strip(), password.strip()
 
@@ -135,21 +136,23 @@ class SiteDocroot(click.ParamType):
     Format: SITENAME,DOCROOT,CREATEDIR[:...]
     Example: siteone.local,siteone,y/html:sitetwo.local,sitetwo,n/html"""
 
-    name = 'site_docroot'
+    name = "site_docroot"
 
     def convert(self, value, param, ctx):
-        sites = value.split(':')
+        sites = value.split(":")
         cleaned_sites = []
         msg = 'must be a sitename, document root and a "y" or "n" (create site dir) seperated by a comma, and sitename must have a . in it'
-        msg = ' '.join(msg.split())
+        msg = " ".join(msg.split())
         for site in sites:
             try:
-                sitename, documentroot, createdir = [i.strip() for i in site.split(',', 2) if i.strip()]
+                sitename, documentroot, createdir = [
+                    i.strip() for i in site.split(",", 2) if i.strip()
+                ]
             except ValueError:
                 self.fail(msg, param, ctx)
             if not is_server(sitename):
                 self.fail(msg, param, ctx)
-            if createdir.lower() not in ['y', 'n']:
+            if createdir.lower() not in ["y", "n"]:
                 self.fail(msg, param, ctx)
             cleaned_sites.append((sitename, documentroot, createdir))
         return cleaned_sites
@@ -160,14 +163,17 @@ SITE_DOCROOT = SiteDocroot()
 
 class UserEmailPass(click.ParamType):
     """Check if a string is a username, email and password seperated by a comma."""
-    name = 'user_email_pass'
+
+    name = "user_email_pass"
 
     def convert(self, value, param, ctx):
-        msg = '''must be a username, email and password seperated by a comma
-                 (the password can have a comma in it, but not the username or email).'''
-        msg = ' '.join(msg.split())
+        msg = """must be a username, email and password seperated by a comma
+                 (the password can have a comma in it, but not the username or email)."""
+        msg = " ".join(msg.split())
         try:
-            username, email, password = [i.strip() for i in value.split(',', 2) if i.strip()]
+            username, email, password = [
+                i.strip() for i in value.split(",", 2) if i.strip()
+            ]
         except ValueError:
             self.fail(msg, param, ctx)
         if not is_email(email):
@@ -179,10 +185,10 @@ USER_EMAIL_PASS = UserEmailPass()
 
 
 class IpAddress(click.ParamType):
-    name = 'ip_address'
+    name = "ip_address"
 
     def convert(self, value, param, ctx):
-        msg = 'Ip address is not vaid'
+        msg = "Ip address is not vaid"
         if not is_ipaddress(value):
             self.fail(msg, param, ctx)
         return value
@@ -194,7 +200,7 @@ IP_ADDRESS = IpAddress()
 def deps(*dependencies):
     # remove the first three arguments and any options so only
     # the wanted modules are left
-    cmd_mods = [i for i in sys.argv if not i.startswith('-')][3:]
+    cmd_mods = [i for i in sys.argv if not i.startswith("-")][3:]
     for i in dependencies:
         if i in cmd_mods:
             return True
@@ -205,9 +211,9 @@ def deps(*dependencies):
 
 CONTEXT_SETTINGS = {
     # add -h in addition to --help
-    'help_option_names': ['-h', '--help'],
+    "help_option_names": ["-h", "--help"],
     # allow case insensitive commands
-    'token_normalize_func': lambda x: x.lower(),
+    "token_normalize_func": lambda x: x.lower(),
 }
 
 
@@ -236,79 +242,142 @@ def boss():
     this server to make use of it."""
 
 
-
 @boss.command()  # no_args_is_help=True # Click 7.1
-@click.argument('servername', type=SERVER)
-@click.argument('modules', nargs=-1, required=True)
-@click.option('-d', '--dry-run', is_flag=True,
-              help='Only print the commands that would be used')
-@click.option('-o', '--no-required', is_flag=True,
-              help="Don't install the required modules")
-@click.option('-O', '--no-dependencies', is_flag=True,
-              help="Don't install dependent modules")
-@click.option('--generate-script', is_flag=True,
-              help='Output suitable for a bash script instead of running them')
-@click.option('--dist-version', type=float,
-              help='The version of Ubuntu to assume instead of autodetect.')
+@click.argument("servername", type=SERVER)
+@click.argument("modules", nargs=-1, required=True)
+@click.option(
+    "-d", "--dry-run", is_flag=True, help="Only print the commands that would be used"
+)
+@click.option(
+    "-o", "--no-required", is_flag=True, help="Don't install the required modules"
+)
+@click.option(
+    "-O", "--no-dependencies", is_flag=True, help="Don't install dependent modules"
+)
+@click.option(
+    "--generate-script",
+    is_flag=True,
+    help="Output suitable for a bash script instead of running them",
+)
+@click.option(
+    "--dist-version",
+    type=float,
+    help="The version of Ubuntu to assume instead of autodetect.",
+)
 # unix user
-@click.option('-n', '--new-user-and-pass', type=USER_PASS, metavar='USERNAME,USERPASS',
-              help="a new unix user's name and password (seperated by a comma), they will be added to the www-data group")
+@click.option(
+    "-n",
+    "--new-user-and-pass",
+    type=USER_PASS,
+    metavar="USERNAME,USERPASS",
+    help="a new unix user's name and password (seperated by a comma), they will be added to the www-data group",
+)
 # mysql
-@click.option('-S', '--sql-file', type=click.Path(exists=True, dir_okay=False), metavar='SQLFILE',
-              help='sql file to be run during install')
-@click.option('-N', '--db-name', metavar='DB-NAME',
-              required=deps('mysql', 'lamp', 'craft3'),
-              help="the name the schema to create")
-@click.option('-P', '--db-root-pass', default="password", metavar='PASSWORD',
-              required=deps('mysql', 'lamp', 'craft3', 'phpmyadmin'),
-              help='password for mysql root user, required for the mysql module')
-@click.option('-A', '--new-db-user-and-pass', type=USER_PASS, metavar='USERNAME,PASSWORD',
-              help="a new db user's new username and password (seperated by a comma)")
+@click.option(
+    "-S",
+    "--sql-file",
+    type=click.Path(exists=True, dir_okay=False),
+    metavar="SQLFILE",
+    help="sql file to be run during install",
+)
+@click.option(
+    "-N",
+    "--db-name",
+    metavar="DB-NAME",
+    required=deps("mysql", "lamp", "craft3"),
+    help="the name the schema to create",
+)
+@click.option(
+    "-P",
+    "--db-root-pass",
+    default="password",
+    metavar="PASSWORD",
+    required=deps("mysql", "lamp", "craft3", "phpmyadmin"),
+    help="password for mysql root user, required for the mysql module",
+)
+@click.option(
+    "-A",
+    "--new-db-user-and-pass",
+    type=USER_PASS,
+    metavar="USERNAME,PASSWORD",
+    help="a new db user's new username and password (seperated by a comma)",
+)
 # new user
-@click.option('-u', '--new-system-user-and-pass', type=USER_PASS, metavar='USERNAME,PASSWORD',
-              required=deps('newuser'),
-              help="a new system user's new username and password (seperated by a comma)")
+@click.option(
+    "-u",
+    "--new-system-user-and-pass",
+    type=USER_PASS,
+    metavar="USERNAME,PASSWORD",
+    required=deps("newuser"),
+    help="a new system user's new username and password (seperated by a comma)",
+)
 # virtualhost
-@click.option('-s', '--site-name-and-root', type=SITE_DOCROOT, metavar='SITENAME,DOCUMENTROOT[:...]',
-              required=deps('virtualhost'),
-              help='''SITENAME, DOCUMENTROOT and CREATEDIR seperated by a comma (doc root will be put in /var/www).
+@click.option(
+    "-s",
+    "--site-name-and-root",
+    type=SITE_DOCROOT,
+    metavar="SITENAME,DOCUMENTROOT[:...]",
+    required=deps("virtualhost"),
+    help="""SITENAME, DOCUMENTROOT and CREATEDIR seperated by a comma (doc root will be put in /var/www).
                 CREATEDIR is an optional y/n that indicates if to create the dir or not (default:n).
-                Multiple sites can be specified by seperating them with a ":", eg: -s site1,root1,y:site2,root2''')
+                Multiple sites can be specified by seperating them with a ":", eg: -s site1,root1,y:site2,root2""",
+)
 # craft 3
-@click.option('-c', '--craft-credentials', type=USER_EMAIL_PASS, metavar='USERNAME,EMAIL,PASSWORD',
-              help='Craft admin credentials. If not set, only system requirements for Craft will be installed')
+@click.option(
+    "-c",
+    "--craft-credentials",
+    type=USER_EMAIL_PASS,
+    metavar="USERNAME,EMAIL,PASSWORD",
+    help="Craft admin credentials. If not set, only system requirements for Craft will be installed",
+)
 # aptproxy
-@click.option('-i', '--host-ip', type=IP_ADDRESS,
-              required=deps('aptproxy'),
-              help='Host ip to be used in aptproxy config')
+@click.option(
+    "-i",
+    "--host-ip",
+    type=IP_ADDRESS,
+    required=deps("aptproxy"),
+    help="Host ip to be used in aptproxy config",
+)
 # netdata
-@click.option('--netdata-user-pass', type=USER_PASS, metavar='USERNAME,USERPASS',
-              help="a new user's name and password (seperated by a comma)")
+@click.option(
+    "--netdata-user-pass",
+    type=USER_PASS,
+    metavar="USERNAME,USERPASS",
+    help="a new user's name and password (seperated by a comma)",
+)
 def install(**args):
     """Install any modules available from `boss list`"""
 
     # convert the args dict to a namedtuple
-    Args = namedtuple('Args', sorted(args))
+    Args = namedtuple("Args", sorted(args))
     args = Args(**args)
+
+    if args.dist_version:
+        global DIST_VERSION
+        DIST_VERSION = args.dist_version
 
     available_mods = mods
     wanted_mods = [i.lower() for i in args.modules]
-    required_mods = ['first', 'done']
+    required_mods = ["first", "done"]
     # extract the requested modules and the required from available_mods list
     if args.no_required:
         wanted_apps = [i for i in available_mods if i.__name__.lower() in wanted_mods]
     else:
-        wanted_apps = [i for i in available_mods if
-                       i.__name__.lower() in wanted_mods or i.__name__.lower() in required_mods]
+        wanted_apps = [
+            i
+            for i in available_mods
+            if i.__name__.lower() in wanted_mods or i.__name__.lower() in required_mods
+        ]
 
     # check if the user is asking for non-existent modules
     mapping_keys = [i.__name__.lower() for i in available_mods]
     invalid_modules = [i for i in wanted_mods if i not in mapping_keys]
     if invalid_modules:
-        util.error('module(s) "{invalid}" does not exist.\nValid modules are:\n{valid}'.format(
-            valid=', '.join(mapping_keys),
-            invalid=', '.join(invalid_modules)
-        ))
+        util.error(
+            'module(s) "{invalid}" does not exist.\nValid modules are:\n{valid}'.format(
+                valid=", ".join(mapping_keys), invalid=", ".join(invalid_modules)
+            )
+        )
 
     # check if the requested modules have their dependencies met
     if not args.no_dependencies:
@@ -318,19 +387,22 @@ def install(**args):
             provided = set(install_reqs)
             required = set(app.requires)
             if len(required - provided):
-                util.error('Requirements not met for {}: {}.'.format(
-                    app.__name__.lower(), ', '.join(app.requires)))
+                util.error(
+                    "Requirements not met for {}: {}.".format(
+                        app.__name__.lower(), ", ".join(app.requires)
+                    )
+                )
 
     if args.generate_script:
         script_header = (
-            '#!/usr/bin/env bash',
-            '',
-            '# Boss command used to generate this script',
-            '# {}'.format(' '.join(sys.argv)),
-            '',
-            'set -x',
+            "#!/usr/bin/env bash",
+            "",
+            "# Boss command used to generate this script",
+            "# {}".format(" ".join(sys.argv)),
+            "",
+            "set -x",
         )
-        click.echo('\n'.join(script_header))
+        click.echo("\n".join(script_header))
 
     for App in wanted_apps:
         module_name = App.title
@@ -356,7 +428,7 @@ def install(**args):
 @boss.command()
 def list():
     """List available modules"""
-    installed_file = os.path.expanduser('~/boss-installed-modules')
+    installed_file = os.path.expanduser("~/boss-installed-modules")
     installed = []
     if os.path.exists(installed_file):
         with open(installed_file) as f:
@@ -367,16 +439,12 @@ def list():
         name = mod.__name__
         module = mod
         # state = '[X] ' if name in installed else '[-] '
-        state = ' ✓ ' if name in installed else '   '
-        state = click.style(state, fg='green')
-        description = module.__doc__ if module.__doc__ else ''
+        state = " ✓ " if name in installed else "   "
+        state = click.style(state, fg="green")
+        description = module.__doc__ if module.__doc__ else ""
         if description:
             description = description.splitlines()[0]
-        click.echo(
-            state +
-            click.style(name.ljust(13), bold=True) +
-            description
-        )
+        click.echo(state + click.style(name.ljust(13), bold=True) + description)
         sys.stdout.flush()
 
 
@@ -385,23 +453,25 @@ def help():
     """Show help for each module"""
 
     content = []
-    w = textwrap.TextWrapper(initial_indent='', subsequent_indent='  ', break_on_hyphens=False)
+    w = textwrap.TextWrapper(
+        initial_indent="", subsequent_indent="  ", break_on_hyphens=False
+    )
     for app in mods:
-        content.append('')
+        content.append("")
 
-        title = '{} ({})'.format(app.title, app.__name__.lower())
-        under = '-' * len(title)
-        content.append(click.style(title, fg='yellow', bold=False, underline=True))
+        title = "{} ({})".format(app.title, app.__name__.lower())
+        under = "-" * len(title)
+        content.append(click.style(title, fg="yellow", bold=False, underline=True))
 
         if app.__doc__:
             lines = app.__doc__.splitlines()
             lines = [i.strip() for i in lines]
-            content.append('\n'.join(lines).strip())
+            content.append("\n".join(lines).strip())
         else:
-            content.append(click.style('(No documentation)', dim=True))
+            content.append(click.style("(No documentation)", dim=True))
         if app.requires:
-            content.append('')
-            cont_title = click.style('Required modules:', fg='blue')
-            content.append('{} {}'.format(cont_title, ', '.join(app.requires)))
-    content.append('\n')
-    click.echo_via_pager('\n'.join(content))
+            content.append("")
+            cont_title = click.style("Required modules:", fg="blue")
+            content.append("{} {}".format(cont_title, ", ".join(app.requires)))
+    content.append("\n")
+    click.echo_via_pager("\n".join(content))
--- src/boss/dist.py
+++ src/boss/dist.py
@@ -1,34 +1,42 @@
 # run-shell-command :: ../build.bash
 
 import distro
+from .util import error
+
 
 class Dist:
-    """Test whether the current distro meets certain version requirments.
+    """Test whether the current distro meets certain version requirements.
 
     d = Dist()
     d == Dist.UBUNTU
     d == (Dist.UBUNTU, Dist.V16_10)
     d > (Dist.UBUNTU, Dist.V16_10)
     """
-    UBUNTU = 'Ubuntu'
+
+    UBUNTU = "Ubuntu"
     V14_04 = 14.04  # Trusty Tahr
     V16_04 = 16.04  # Xenial Xerus
     V18_04 = 18.04  # Bionic Beaver
     V20_04 = 20.04  # Focal Fossa
     V22_04 = 22.04  # Jammy Jellyfish
     V24_04 = 24.04  # Oracular Oriole
+    version: float
 
-    def __init__(self, version:float|None=None) -> None:
+    def __init__(self) -> None:
         self.name = distro.name()
-        version = 24.04
-        if version:
-            self.version = str(version)
+
+        try:
+            from .cli import DIST_VERSION
+        except ImportError:
+            error("Could not import DIST_VERSION from cli module.")
+
+        if DIST_VERSION:
+            self.version = DIST_VERSION
         else:
-            self.version = distro.version()
-        print(distro.version())
+            self.version = float(distro.version())
 
     def __str__(self) -> str:
-        return '{name} {version}'.format(**self.__dict__)
+        return "{name} {version}".format(**self.__dict__)
 
     def __eq__(self, other):
         """Comparison can be done with the distro name or the distro name and version.
@@ -42,16 +50,16 @@ class Dist:
             return self.name == other
 
     def __ne__(self, other):
-        return self.name != other[0] or float(self.version) != float(other[1])
+        return self.name != other[0] or self.version != float(other[1])
 
     def __lt__(self, other):
-        return self.name == other[0] and float(self.version) < float(other[1])
+        return self.name == other[0] and self.version < float(other[1])
 
     def __le__(self, other):
-        return self.name == other[0] and float(self.version) <= float(other[1])
+        return self.name == other[0] and self.version <= float(other[1])
 
     def __gt__(self, other):
-        return self.name == other[0] and float(self.version) > float(other[1])
+        return self.name == other[0] and self.version > float(other[1])
 
     def __ge__(self, other):
-        return self.name == other[0] and float(self.version) >= float(other[1])
+        return self.name == other[0] and self.version >= float(other[1])
