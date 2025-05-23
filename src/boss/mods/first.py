20e8ebb5f08c9cadaf4b497d4aeb6f1221a9e409
Sheldon McGrandle <developer@8cylinder.com>
2025-05-23 09:27:44 -0700

    Do some light formatting

--- src/boss/mods/first.py
+++ src/boss/mods/first.py
@@ -8,53 +8,84 @@ from ..errors import *
 class First(Bash):
     """Misc apps that are useful
 
-    The timezone is set to America/Los_Angeles and Emacs is configured
-    as the defalt editor.
+    The timezone is set to "America/Los_Angeles" and Emacs is configured
+    as the default editor.
     """
-    provides = ['first']
+
+    provides = ["first"]
     requires = []
-    title = 'First'
+    title = "First"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.distro > (Dist.UBUNTU, Dist.V14_04):
             self.apt_pkgs = [
-                'tree', 'elinks', 'virt-what', 'silversearcher-ag', 'unzip',
-                'zip', 'htop', 'source-highlight', 'whois', 'curl', 'figlet', 'ntp', 'locate'
+                "tree",
+                "elinks",
+                "virt-what",
+                "silversearcher-ag",
+                "unzip",
+                "zip",
+                "htop",
+                "source-highlight",
+                "whois",
+                "curl",
+                "figlet",
+                "ntp",
+                "locate",
             ]
             # self.apt_pkgs += ['joe']
-            self.apt_pkgs += ['emacs-nox'] # adds aprox 100mb
+            self.apt_pkgs += ["emacs-nox"]  # adds aprox 100mb
         elif self.distro == (Dist.UBUNTU, Dist.V14_04):
             self.apt_pkgs = [
-                'tree', 'elinks', 'virt-what', 'silversearcher-ag', 'unzip',
-                'htop', 'source-highlight', 'whois', 'curl', 'figlet'
+                "tree",
+                "elinks",
+                "virt-what",
+                "silversearcher-ag",
+                "unzip",
+                "htop",
+                "source-highlight",
+                "whois",
+                "curl",
+                "figlet",
             ]
             # self.apt_pkgs += ['joe']
-            self.apt_pkgs += ['emacs24-nox'] # adds aprox 100mb
+            self.apt_pkgs += ["emacs24-nox"]  # adds aprox 100mb
 
     def pre_install(self):
         pass
 
     def post_install(self):
         # set timezone
-        tz = 'America/Los_Angeles'
-        self.run('sudo timedatectl set-timezone {}'.format(tz))
+        tz = "America/Los_Angeles"
+        self.run("sudo timedatectl set-timezone {}".format(tz))
 
         self.bash_settings()
 
     def bash_settings(self):
         bashrc = "$HOME/.bashrc"
-        editor = 'emacs'
-        settings = '''
+        editor = "emacs"
+        settings = """
+          PS1='\n${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ '
+
+          alias ls='LC_ALL=C ls --almost-all --classify --human-readable --color=auto --group-directories-first'
+          alias time='/usr/bin/time --format="Time elapsed: %E"'
+          alias pss='ps -Af | grep -i $1'
+          alias grep='grep --color=auto'
+          alias tree='tree --dirsfirst'
+          alias gs='git status'
+          alias gl='git log --name-only'
+          alias gls='git log --pretty=format:"%ad  %s" --date=short'
+
           export HISTSIZE=-1
           export HISTFILESIZE=-1
           export HISTTIMEFORMAT="%F %T "
           shopt -s histappend
 
-          export EDITOR='{editor}'
-          export VISUAL='{editor}'
-          export SUDO_EDITOR='{editor}'
-        '''.format(editor=editor)
+          export EDITOR=emacs
+          export VISUAL=emacs
+          export SUDO_EDITOR=emacs
+        """
 
-        settings = '\n'.join([i[10:] for i in settings.split('\n')])
+        settings = "\n".join([i[10:] for i in settings.split("\n")])
         self.append_to_file(bashrc, settings, backup=True)
--- src/boss/mods/newuser.py
+++ src/boss/mods/newuser.py
@@ -1,7 +1,6 @@
 # run-shell-command :: ../../build.bash
 
 from ..bash import Bash
-from ..dist import Dist
 from ..errors import *
 
 
@@ -11,9 +10,9 @@ class NewUser(Bash):
     Create a new user and add them to the sudo and www-data groups.
     The new user does not require a password for sudo."""
 
-    provides = ['newuser']
-    requires = ['first']
-    title = 'New user'
+    provides = ["newuser"]
+    requires = ["first"]
+    title = "New user"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -21,18 +20,24 @@ class NewUser(Bash):
     def pre_install(self):
         username, password = self.args.new_system_user_and_pass
 
-        self.run('if ! id -u {username} &>/dev/null; then sudo useradd --shell=/bin/bash --create-home --password \
-                  $(mkpasswd -m sha-512 {password}) {username}; fi'.format(
-            password=password, username=username
-        ))
-        self.run('### or if using these commands interactively, use:')
-        self.run('# adduser {username}...'.format(username=username, password=password))
+        self.run(
+            "if ! id -u {username} &>/dev/null; then sudo useradd --shell=/bin/bash --create-home --password \
+                  $(mkpasswd -m sha-512 {password}) {username}; fi".format(
+                password=password, username=username
+            )
+        )
+        self.run("### or if using these commands interactively, use:")
+        self.run("# adduser {username}...".format(username=username))
         # add user to some groups
-        for group in ('sudo', 'www-data'):
-            self.run('sudo usermod -aG {group} {username}'.format(
-                group=group,
-                username=username
-            ))
+        for group in ("sudo", "www-data"):
+            self.run(
+                "sudo usermod -aG {group} {username}".format(
+                    group=group, username=username
+                )
+            )
         # make user not need a password for sudo
-        sudo_file = '/etc/sudoers.d/{}-{}'.format(self.scriptname, username)  # filename cannot have a . or ~
-        self.run("echo '{} ALL=(ALL) NOPASSWD:ALL' | sudo tee {}".format(username, sudo_file))
+        # filename cannot have a . or ~
+        sudo_file = "/etc/sudoers.d/{}-{}".format(self.scriptname, username)
+        self.run(
+            "echo '{} ALL=(ALL) NOPASSWD:ALL' | sudo tee {}".format(username, sudo_file)
+        )
